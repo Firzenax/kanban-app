f5db228a804a07ebd49575a7fe5a55d0168aa24d
Firzenax <louis.20160437@gmail.com>
2024-04-03 16:06:07 +0200

    project architecture

--- kanban-app/src/app/layout.tsx
+++ kanban-app/src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Providers } from "@/components/redux/provider";
+import Navbar from "./components/NavBar";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const pjs = Plus_Jakarta_Sans({ subsets: ["latin"], display: "swap" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -11,12 +13,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={pjs.className}>
+      <body>
+        <Providers>
+          <Navbar /> {/* Render the component here */}
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
